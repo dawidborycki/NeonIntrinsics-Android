5269478b7f632e030286a9144937e994aef57c59
Dawid Borycki <dawid@borycki.com.pl>
2020-04-20 19:04:52 +0200

    Initial commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,116 @@
+#include <jni.h>
+#include <string>
+#include <arm_neon.h>
+#include <chrono>
+
+using namespace std;
+
+short* generateRamp(short startValue, short len) {
+    short* ramp = new short[len];
+
+    for(short i = 0; i < len; i++) {
+        ramp[i] = startValue + i;
+    }
+
+    return ramp;
+}
+
+double msElapsedTime(chrono::system_clock::time_point start) {
+    auto end = chrono::system_clock::now();
+
+    return chrono::duration_cast<chrono::milliseconds>(end - start).count();
+}
+
+chrono::system_clock::time_point now() {
+    return chrono::system_clock::now();
+}
+
+int dotProduct(short* vector1, short* vector2, short len) {
+    int result = 0;
+
+    for(short i = 0; i < len; i++) {
+        result += vector1[i] * vector2[i];
+    }
+
+    return result;
+}
+
+int dotProductNeon(short* vector1, short* vector2, short len) {
+    const short transferSize = 4;
+    short segments = len / transferSize;
+
+    // 4-element vector of zeros
+    int32x4_t partialSumsNeon = vdupq_n_s32(0);
+
+    // Main loop (note that loop index goes through segments)
+    for(short i = 0; i < segments; i++) {
+        // Load vector elements to registers
+        short offset = i * transferSize;
+        int16x4_t vector1Neon = vld1_s16(vector1 + offset);
+        int16x4_t vector2Neon = vld1_s16(vector2 + offset);
+
+        // Multiply and accumulate: partialSumsNeon += vector1Neon * vector2Neon
+        partialSumsNeon = vmlal_s16(partialSumsNeon, vector1Neon, vector2Neon);
+    }
+
+    // Store partial sums
+    int partialSums[transferSize];
+    vst1q_s32(partialSums, partialSumsNeon);
+
+    // Sum up partial sums
+    int result = 0;
+    for(short i = 0; i < transferSize; i++) {
+        result += partialSums[i];
+    }
+
+    return result;
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_neonintrinsics_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+
+    // Ramp length and number of trials
+    const int rampLength = 1024;
+    const int trials = 10000;
+
+    // Generate two input vectors
+    // (0, 1, ..., rampLength - 1)
+    // (100, 101, ..., 100 + rampLength-1)
+    auto ramp1 = generateRamp(0, rampLength);
+    auto ramp2 = generateRamp(100, rampLength);
+
+    // Without NEON intrinsics
+    // Invoke dotProduct and measure performance
+    int lastResult = 0;
+
+    auto start = now();
+    for (int i = 0; i < trials; i++) {
+        lastResult = dotProduct(ramp1, ramp2, rampLength);
+    }
+    auto elapsedTime = msElapsedTime(start);
+
+    // With NEON intrinsics
+    // Invoke dotProductNeon and measure performance
+    int lastResultNeon = 0;
+
+    start = now();
+    for (int i = 0; i < trials; i++) {
+        lastResultNeon = dotProductNeon(ramp1, ramp2, rampLength);
+    }
+    auto elapsedTimeNeon = msElapsedTime(start);
+
+    // Clean up
+    delete ramp1, ramp2;
+
+    // Display results
+    std::string resultsString =
+            +"Result: " + to_string(lastResult)
+            + "\nElapsed time: " + to_string((int) elapsedTime) + " ms"
+            + "\n\n----==== NEON ====----\n"
+            + "Result: " + to_string(lastResultNeon)
+            + "\nElapsed time: " + to_string((int) elapsedTimeNeon) + " ms";
+
+    return env->NewStringUTF(resultsString.c_str());
+}
